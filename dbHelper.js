b882f5268b65cad229f2b6a2e3dfb66e37f9fd9a
Maciej <mitek.chmielewski@gmail.com>
2022-11-09 21:58:21 +0100

    Initial commit

---
+++ dbHelper.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require("mongodb");
+
+const encodedDbPassword = encodeURIComponent(process.env.DB_PASS);
+
+const connectionString = `mongodb://${process.env.DB_NAME}:${encodedDbPassword}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
+
+const client = new MongoClient(connectionString);
+
+//I dont get why it doesnt get garbage collected
+let db;
+
+module.exports = {
+  connectToServer: function (callback) {
+    client
+      .connect()
+      .then((connection) => {
+        db = connection.db(process.env.DB_NAME);
+        console.log("Successfully connected to MongoDB.");
+        return callback();
+      })
+      .catch((err) => console.log(err));
+  },
+  getDb: function () {
+    return db;
+  },
+};
